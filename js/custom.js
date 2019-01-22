23dac97ae03660253b80bee8ffb33e3782fa372a
Miavega <miguel_9504@hotmail.com>
2019-01-22 17:50:54 -0500

    update js settings

--- js/custom.js
+++ js/custom.js
@@ -5,7 +5,7 @@ $('.nav-link').click(function () {
 });
 
 // NAVEGATE PAGE
-$('a[href*=\\#]:not([href=\\#carouselExampleIndicators])').click(function () {
+$('a[href*=\\#]:not([href=\\#carouselExampleIndicators],[href=\\#exampleModal])').click(function () {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
         var target = $(this.hash);
         target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
